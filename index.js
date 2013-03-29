e1d0893ea7b7cb93954d3a5d2c6fff0bdee1b153
Steve Perkins <stereosteve@gmail.com>
2013-03-29 15:30:15 -0400

    use stereosteve/SoundManager2

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+require('SoundManager2');
+
+
 
 module.exports = Player;
 function Player() {
@@ -5,22 +8,28 @@ function Player() {
   this.el.textContent = 'el player';
   this.el.classList.add('player');
 
+  var self = this;
+
   var url = "https://cloudup-files.s3.amazonaws.com/1355244692571.845b8dfc324b7bd3c548e8c06380908e";
 
 
   var sm2 = soundManager;
   sm2.setup({
-    url: '/sm2',
+    url: '/build/stereosteve-SoundManager2/swf',
     debugMode: false,
     flashVersion: 9,
-    onready: function() {
-      console.log('onready');
-      var sound = sm2.createSound({
-        id: url,
-        url: url,
-        autoPlay: true
-      });
-    }
+    onready: playSound
   })
-  console.log(sm2.setupOptions, 'dagg');
+
+  function playSound() {
+    var sound = sm2.createSound({
+      id: url,
+      url: url,
+      autoPlay: true,
+      whileplaying: function() {
+        self.el.textContent = this.position;
+      }
+    });
+  }
+
 }
